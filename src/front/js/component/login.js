c19baff33b1be1cc659e1bd90f20b9849e26d0c6
Alberto Limón <119005684+albertolimongithub@users.noreply.github.com>
2024-06-27 18:44:15 +0000

    fc

---
+++ src/front/js/component/login.js
@@ -0,0 +1,36 @@
+import { useContext } from "react";
+import { Context } from "../store/appContext";
+
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+
+export const Login = () => {
+    
+
+	const [inputEmail, setInputEmail] = useState("");
+	const [inputPassword, setInputPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <div className="container-fluid">
+			<h1 className="mb-4">LOGIN</h1>
+				<form className="formulario">
+		
+
+					<label htmlFor="emailUser" className="form-label">Email</label>
+					<input type="email" className="form-control w-50 mb-3" id="emailInput" placeholder="Enter email" onChange={(event) => setInputEmail(event.target.value)}
+							value={inputEmail}/>
+
+					<label htmlFor="passwordUser" className="form-label">Address</label>
+					<input type="password" className="form-control w-50 mb-3" id="passwordInput" placeholder="Enter password" onChange={(event) => setInputPassword(event.target.value)}
+							value={inputPassword}/>
+	
+					<button type="submit" className="btn btn-primary w-50 mb-3" onClick>Sign up</button>
+					
+				</form>
+		    </div>
+        </>
+    )
+}
